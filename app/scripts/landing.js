3e576ea2aa957bf829867d3e09e7de17e1407819
namuxco <namydesign@gmail.com>
2014-08-31 21:53:54 -0600

    Added jQuery animations to landing page

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -4,6 +4,14 @@
       $(this).text(subText + "!");
      });
 
+  $('h3').hover(function(){
+    console.log('change font color on');
+    $(this).css({'color': 'pink'});
+    },function(){
+    console.log('change font color off');
+    $(this).css({'color': 'white'});
+});
+
    var onHoverAction = function(event) {
      console.log('Hover action triggered.');
      $(this).animate({'margin-top': '10px'});
@@ -15,4 +23,14 @@
    };
  
    $('.selling-points .point').hover(onHoverAction, offHoverAction);
+
+   $('.selling-points h5').click(function(){
+    console.log('font size increase upon click');
+    $(this).css({'font-size': '150%'})
+   })
+
+   $('.player-header-nav').click(function(){
+    console.log('fade out');
+    $(this).fadeOut(1000);
+   })
  });
